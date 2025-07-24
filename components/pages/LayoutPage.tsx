3066a910a1c5796fb4adb312f5a851f47f7a652e
McKay <mckayjunk@gmail.com>
2025-07-24 07:23:21 -0600

    agina

--- components/pages/LayoutPage.tsx
+++ components/pages/LayoutPage.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useEffect, useRef } from 'react';
 import { useSettings } from '../../context/SettingsContext';
 import { useAppState, useAppDispatch } from '../../context/AppContext';

951e75bdd1306b158d93167aa243797083eb8122
McKay <mckayjunk@gmail.com>
2025-07-29 11:19:07 -0600

    Update entrypoint file

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,10 @@
 set -e
 
 # Take ownership of the volume mounts. This is run as root before we drop privileges.
-chown -R appuser:appgroup /app/uploads /app/backend/backups /app/backend/data.db
+chown -R appuser:appgroup /app/uploads /app/backend/backups /app/backend/db
+
+# Create db file
+touch /app/backend/data.db
 
 # Execute the main container command (CMD) as the non-root 'appuser'
 exec su-exec appuser "$@"
